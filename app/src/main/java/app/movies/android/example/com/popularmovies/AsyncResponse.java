55010156f64b19b24e82efd8636310b60799f6d2
mnarasimhan <maheshya@gmail.com>
2016-09-24 20:19:28 -0400

    Phase 1 movie app complete

---
+++ app/src/main/java/app/movies/android/example/com/popularmovies/AsyncResponse.java
@@ -0,0 +1,19 @@
+package app.movies.android.example.com.popularmovies;
+
+/**
+ * Created by Mahesh on 9/22/2016.
+ */
+import java.util.List;
+
+/*
+*
+* Custom interface to implement a call back function that returns results
+* from an async task
+*
+* */
+
+public interface AsyncResponse {
+
+    void onTaskCompleted( List<Movie> results );
+
+}
---
+++ app/src/main/java/app/movies/android/example/com/popularmovies/MainActivity.java
@@ -0,0 +1,63 @@
+package app.movies.android.example.com.popularmovies;
+import android.app.FragmentManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+/*
+*
+* Main activity and entry point into the app
+* Inflates MovieListFragment for the main UI
+*
+* */
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
+    private FragmentManager fragmentManager = getFragmentManager();
+    MovieListFragment fragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            fragment = new MovieListFragment();
+            fragmentManager.beginTransaction()
+                    .add(R.id.container, fragment)
+                    .commit();
+        }else{
+            fragment = (MovieListFragment) fragmentManager.getFragment(
+                    savedInstanceState, "fragmentContent");
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        fragmentManager.putFragment(savedInstanceState, "fragmentContent", fragment);
+    }
+
+}
